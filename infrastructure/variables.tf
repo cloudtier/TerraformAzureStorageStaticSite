9c66f29f1d3e9f0f135cb29e8bd901661cefa56a
cloudtier <67243480+cloudtier@users.noreply.github.com>
2022-06-05 19:54:07 +0100

    Create variables.tf

---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+# variables.tf
+variable "domain" {
+  type    = string
+  default = "<YOUR_DOMAIN>"
+}
+
+variable "cdn_application_id" {
+  default = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" # This is azure's application UUID for a CDN endpoint
+}
+
+variable "regions" {
+  type = map(string)
+  default = {
+    "primary" = "<REGION>"
+    "cdn"     = "<CDN_REGION>"
+  }
+}
